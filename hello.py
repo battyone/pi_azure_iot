eefd5d39102d0990eccdf8ff3b299095ad4623e4
Chatdanai Lumdee <chatdanai.l@gmail.com>
2020-04-11 17:52:32 +0700

    add config.py to .gitignore

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 print('hello')
 
-msg = 'hello'
+msg = 'hello world'
 print(msg.capitalize())
 
 import numpy as np 
 np.random.choice([1,2,3])
 
 print('hi')
+print('hihi')
+
+
+import os
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
