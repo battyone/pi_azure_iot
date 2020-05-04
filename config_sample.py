b401e83c151014344dcb2fb762f1936c2922cd71
Chatdanai Lumdee <26705716+clumdee@users.noreply.github.com>
2020-05-04 11:54:15 +0700

    Upload sample config

---
+++ config_sample.py
@@ -0,0 +1,20 @@
+# admin
+username = "username"
+password = "password"
+
+# azure
+resource_group = "YOUR AZURE RESOURCE GROUP NAME"
+iot_hub        = "YOUR AZURE IOT HUB NAME"
+
+# iot-hub
+device_name    = "YOUR DEVICE_NAME IN IOT HUB TO USE AS COMMUNICATION CHANNEL"
+device_connect = "CONNECTION STRING TO DEVICE IN IOT HUB"
+
+# sql database
+sql_server     = 'YOUR AZURE SQL SERVER'
+sql_db         = 'YOUR AZURE SQL DATABASE'
+sql_username   = 'USERNAME TO YOUR SQL SERVER'
+sql_password   = 'PASSWORD TO YOUR SQL SERVER'
+
+# bluetooth devices
+btle_devices   = {'device_01': 'AA:11:BB:22:CC:33', 'device_02': 'XX:77:YY:88:ZZ:99'}
